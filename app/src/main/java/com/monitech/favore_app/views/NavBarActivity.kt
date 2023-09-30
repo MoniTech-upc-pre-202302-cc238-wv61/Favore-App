f6c8a0ee7b6dcdee97be1898a82178b27fe8d2d2
Josef Romero Florida <josef.romeroflorida@gmail.com>
2023-09-30 05:21:53 -0500

    feat: add navbar

---
+++ app/src/main/java/com/monitech/favore_app/views/NavBarActivity.kt
@@ -0,0 +1,14 @@
+package com.monitech.favore_app.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.monitech.favore_app.R
+
+class NavBarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navbar)
+
+    }
+}
