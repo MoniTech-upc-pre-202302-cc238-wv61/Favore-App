01705213908c0a7b851f394939e06ae38707a658
jhos <jhos.ricardo14@gmail.com>
2023-09-30 07:04:50 -0500

    feat: add initial sign up funct

--- app/src/main/java/com/monitech/favore_app/views/SignUpActivity.kt
+++ app/src/main/java/com/monitech/favore_app/views/SignUpActivity.kt
@@ -1,16 +1,21 @@
 package com.monitech.favore_app.views
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
+import com.google.android.material.textfield.TextInputLayout
 import com.monitech.favore_app.R
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        var selected = "";
 
         val btnReturn: ImageButton = findViewById(R.id.btnReturn)
         btnReturn.setOnClickListener(){
@@ -26,9 +31,38 @@ class SignUpActivity : AppCompatActivity() {
         val btnClient: Button = findViewById(R.id.btnClient)
         val btnFreelancer: Button = findViewById(R.id.btnFreelancer)
 
+        btnClient.setOnClickListener(){
+            //change the color of the button
+            btnClient.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.md_theme_light_secondary));
+            btnClient.setTextColor(resources.getColor(R.color.white));
+            if(selected=="FREELANCER"){
+                btnFreelancer.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.white));
+                btnFreelancer.setTextColor(resources.getColor(R.color.md_theme_light_secondary));
+            }
+            selected = "CLIENT";
+        }
+
+        btnFreelancer.setOnClickListener(){
+            btnFreelancer.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.md_theme_light_secondary));
+            btnFreelancer.setTextColor(resources.getColor(R.color.white));
+            if(selected=="CLIENT"){
+                btnClient.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.white));
+                btnClient.setTextColor(resources.getColor(R.color.md_theme_light_secondary));
+            }
+            selected = "FREELANCER";
+        }
+
+        val nameTextField: TextInputLayout = findViewById(R.id.nameTextField)
+        val emailTextField: TextInputLayout = findViewById(R.id.emailTextField)
+        val passwordTextField: TextInputLayout = findViewById(R.id.passwordTextField)
 
         val btnSignUp: Button = findViewById(R.id.btnSignUp)
         btnSignUp.setOnClickListener(){
+            Log.d("selected", selected)
+            Log.d("name", nameTextField.editText?.text.toString())
+            Log.d("email", emailTextField.editText?.text.toString())
+            Log.d("password", passwordTextField.editText?.text.toString())
+
         }
     }
 }
