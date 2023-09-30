e7f3fbe855e921b4f4d44d44b66096279f836bc3
jhos <jhos.ricardo14@gmail.com>
2023-09-30 06:26:54 -0500

    feat: add user holder api

---
+++ app/src/main/java/com/monitech/favore_app/interface/UserHolderApi.kt
@@ -0,0 +1,13 @@
+package com.monitech.favore_app.`interface`
+
+import com.monitech.favore_app.dto.UserLoginDTO
+import com.monitech.favore_app.models.User
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface UserHolderApi {
+
+    @POST("users")
+    fun login(@Body userCreateDTO: UserCreateDTO):Call<User>
+}
