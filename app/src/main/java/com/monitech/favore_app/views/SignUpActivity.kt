95bc2359895346ce04bc465f752f2bcd4ddcdb8e
jhos <jhos.ricardo14@gmail.com>
2023-09-30 04:02:19 -0500

    add sign up view

--- app/src/main/java/com/monitech/favore_app/views/SignUpActivity.kt
+++ app/src/main/java/com/monitech/favore_app/views/SignUpActivity.kt
@@ -1,12 +1,26 @@
 package com.monitech.favore_app.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import com.monitech.favore_app.R
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val btnReturn: ImageButton = findViewById(R.id.btnReturn)
+        btnReturn.setOnClickListener(){
+            finish()
+        }
+
+        val btnSignIn: Button = findViewById(R.id.btnSignIn)
+        btnSignIn.setOnClickListener(){
+            val instance = Intent(this, SignInClientActivity::class.java)
+            startActivity(instance)
+        }
     }
 }
