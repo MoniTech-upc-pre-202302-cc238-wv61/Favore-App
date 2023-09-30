f295df2ad2354dac229e6b827579711297ccb756
Josef Romero Florida <josef.romeroflorida@gmail.com>
2023-09-30 06:17:33 -0500

    feat: add search result navbar activity

--- app/src/main/java/com/monitech/favore_app/views/SearchResultsActivity.kt
+++ app/src/main/java/com/monitech/favore_app/views/SearchResultsActivity.kt
@@ -1,12 +1,43 @@
 package com.monitech.favore_app.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.monitech.favore_app.R
 
 class SearchResultsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_results)
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigation.selectedItemId = R.id.navigation_home
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    startActivity(Intent(this, ClientHomeActivity::class.java))
+                    finish()
+                }
+                R.id.navigation_orders -> {
+                    startActivity(Intent(this, FreelancerFavorsManagementActivity::class.java))
+                    finish()
+                }
+                R.id.navigation_search -> {
+                    startActivity(Intent(this, SearchServiceActivity::class.java))
+                    finish()
+                }
+                R.id.navigation_inbox -> {
+                    startActivity(Intent(this, AddFavorActivity::class.java))
+                    finish()
+                }
+                R.id.navigation_user -> {
+                    startActivity(Intent(this, SignInClientActivity::class.java))
+                    finish()
+                }
+            }
+            true
+        }
     }
 }
