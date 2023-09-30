5bbd8dc74051bd478cf1a68a233494c36bdbfff5
jhos <jhos.ricardo14@gmail.com>
2023-09-30 04:31:21 -0500

    feat: add start activity as default

--- app/src/main/java/com/monitech/favore_app/MainActivity.kt
+++ app/src/main/java/com/monitech/favore_app/MainActivity.kt
@@ -12,23 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val btnDemoFreelancerHomepage:Button = findViewById(R.id.btnDemoFreelancerHomepage)
-        val btnDemoClientHomepage: Button = findViewById(R.id.btnDemoClientHomepage)
-        val btnDemoLogin: Button = findViewById(R.id.btnDemoLogin)
-
-        btnDemoFreelancerHomepage.setOnClickListener(){
-            val instance = Intent(this, FreelancerHomeActivity::class.java)
-            startActivity(instance)
-        }
-        btnDemoClientHomepage.setOnClickListener(){
-            val instance = Intent(this, ClientHomeActivity::class.java)
-            startActivity(instance)
-        }
-
-        btnDemoLogin.setOnClickListener(){
-            val instance = Intent(this, StartActivity::class.java)
-            startActivity(instance)
-        }
+        finish()
+        val instance = Intent(this, StartActivity::class.java)
+        startActivity(instance)
     }
 }
