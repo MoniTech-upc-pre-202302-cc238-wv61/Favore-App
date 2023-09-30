b34f8df60eba963f002a2c0468be29a41ecfe496
Josef Romero Florida <josef.romeroflorida@gmail.com>
2023-09-30 06:04:12 -0500

    feat: add navbar to ClientHomeActivity

--- app/src/main/java/com/monitech/favore_app/views/ClientHomeActivity.kt
+++ app/src/main/java/com/monitech/favore_app/views/ClientHomeActivity.kt
@@ -3,6 +3,7 @@ package com.monitech.favore_app.views
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.monitech.favore_app.R
 import com.google.android.material.search.SearchBar
 
@@ -17,6 +18,35 @@ class ClientHomeActivity : AppCompatActivity() {
         searchBar.setOnClickListener {
             val intent = Intent(this, SearchServiceActivity::class.java)
             startActivity(intent)
+        }
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigation.selectedItemId = R.id.navigation_home
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    startActivity(Intent(this, ClientHomeActivity::class.java))
+                    finish()
+                }
+                R.id.navigation_orders -> {
+                    startActivity(Intent(this, FreelancerFavorsManagementActivity::class.java))
+                    finish()
+                }
+                R.id.navigation_search -> {
+                    startActivity(Intent(this, SearchServiceActivity::class.java))
+                    finish()
+                }
+                R.id.navigation_inbox -> {
+                    startActivity(Intent(this, AddFavorActivity::class.java))
+                    finish()
+                }
+                R.id.navigation_user -> {
+                    startActivity(Intent(this, SignInClientActivity::class.java))
+                    finish()
+                }
+            }
+            true
         }
     }
 }
