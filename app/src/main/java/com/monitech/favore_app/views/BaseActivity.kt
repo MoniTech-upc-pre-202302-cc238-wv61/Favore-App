b224e8e72d19481f467eb8c56363b923087a55b5
Josef Romero Florida <josef.romeroflorida@gmail.com>
2023-11-25 02:15:21 -0500

    The Last Hope: The revenge of the navBar

--- app/src/main/java/com/monitech/favore_app/views/BaseActivity.kt
+++ app/src/main/java/com/monitech/favore_app/views/BaseActivity.kt
@@ -1,5 +1,6 @@
 package com.monitech.favore_app.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -43,12 +44,42 @@ open class BaseActivity : AppCompatActivity() {
     private fun setUpBottomNavigationListener(bottomNavigationView: BottomNavigationView) {
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.navigation_home -> {
+                R.id.navigation_home_client -> {
+                    val intent = Intent(this, ClientHomeActivity::class.java)
+                    startActivity(intent)
+                    true
                 }
-                R.id.navigation_orders -> {
+                R.id.navigation_home_freelancer -> {
+                    val intent = Intent(this, FreelancerHomeActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.navigation_orders_client -> {
+                    val intent = Intent(this, ClientContractsManagement::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.navigation_orders_freelancer -> {
+                    val intent = Intent(this, FreelancerManageContracts::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.navitagion_favors_freelancer -> {
+                    val intent = Intent(this, FreelancerFavorsManagementActivity::class.java)
+                    startActivity(intent)
+                    true
                 }
                 R.id.navigation_search -> {
+                    val intent = Intent(this, SearchServiceActivity::class.java)
+                    startActivity(intent)
+                    true
                 }
+                R.id.navigation_user -> {
+                    val intent = Intent(this, UserProfileActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
             }
             true
         }
